965fd03a0f1cd5e9f64fa42cf190ec90691d00bf
caoyong <168690252@qq.com>
2018-12-17 20:21:31 +0800

    2

---
+++ application/common.php
@@ -0,0 +1,173 @@
+<?php
+// +----------------------------------------------------------------------
+// | Yzncms [ 御宅男工作室 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018 http://yzncms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 御宅男 <530765310@qq.com>
+// +----------------------------------------------------------------------
+
+// +----------------------------------------------------------------------
+// | 全局函数文件
+// +----------------------------------------------------------------------
+
+/**
+ * 系统缓存缓存管理
+ * cache('model') 获取model缓存
+ * cache('model',null) 删除model缓存
+ * @param mixed $name 缓存名称
+ * @param mixed $value 缓存值
+ * @param mixed $options 缓存参数
+ * @return mixed
+ */
+function cache($name, $value = '', $options = null)
+{
+    static $cache = '';
+    if (empty($cache)) {
+        $cache = \libs\Cache_factory::instance();
+    }
+    // 获取缓存
+    if ('' === $value) {
+        if (false !== strpos($name, '.')) {
+            $vars = explode('.', $name);
+            $data = $cache->get($vars[0]);
+            return is_array($data) ? $data[$vars[1]] : $data;
+        } else {
+            return $cache->get($name);
+        }
+    } elseif (is_null($value)) {
+//删除缓存
+        return $cache->remove($name);
+    } else {
+//缓存数据
+        if (is_array($options)) {
+            $expire = isset($options['expire']) ? $options['expire'] : null;
+        } else {
+            $expire = is_numeric($options) ? $options : null;
+        }
+        return $cache->set($name, $value, $expire);
+    }
+}
+
+/**
+ * 数据签名认证
+ * @param  array  $data 被认证的数据
+ * @return string       签名
+ */
+function data_auth_sign($data)
+{
+    //数据类型检测
+    if (!is_array($data)) {
+        $data = (array) $data;
+    }
+    ksort($data); //排序
+    $code = http_build_query($data); //url编码并生成query字符串
+    $sign = sha1($code); //生成签名
+    return $sign;
+}
+
+/**
+ * 解析配置
+ * @param string $value 配置值
+ * @return array|string
+ */
+function parse_attr($value = '')
+{
+    $array = preg_split('/[,;\r\n]+/', trim($value, ",;\r\n"));
+    if (strpos($value, ':')) {
+        $value = array();
+        foreach ($array as $val) {
+            list($k, $v) = explode(':', $val);
+            $value[$k] = $v;
+        }
+    } else {
+        $value = $array;
+    }
+    return $value;
+}
+
+/**
+ * 时间戳格式化
+ * @param int $time
+ * @return string 完整的时间显示
+ */
+function time_format($time = null, $format = 'Y-m-d H:i')
+{
+    $time = $time === null ? $_SERVER['REQUEST_TIME'] : intval($time);
+    return date($format, $time);
+}
+
+/**
+ * 把返回的数据集转换成Tree
+ * @param array $list 要转换的数据集
+ * @param string $pid parent标记字段
+ * @param string $level level标记字段
+ * @return array
+ * @author 麦当苗儿 <zuojiazi@vip.qq.com>
+ */
+function list_to_tree($list, $pk = 'id', $pid = 'parentid', $child = '_child', $root = 0)
+{
+    // 创建Tree
+    $tree = array();
+    if (is_array($list)) {
+        // 创建基于主键的数组引用
+        $refer = array();
+        foreach ($list as $key => $data) {
+            $refer[$data[$pk]] = &$list[$key];
+        }
+        foreach ($list as $key => $data) {
+            // 判断是否存在parent
+            $parentId = $data[$pid];
+            if ($root == $parentId) {
+                $tree[] = &$list[$key];
+            } else {
+                if (isset($refer[$parentId])) {
+                    $parent = &$refer[$parentId];
+                    $parent[$child][] = &$list[$key];
+                }
+            }
+        }
+    }
+    return $tree;
+}
+
+/**
+ * 格式化字节大小
+ * @param  number $size      字节数
+ * @param  string $delimiter 数字和单位分隔符
+ * @return string            格式化后的带单位的大小
+ */
+function format_bytes($size, $delimiter = '')
+{
+    $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
+    for ($i = 0; $size >= 1024 && $i < 5; $i++) {
+        $size /= 1024;
+    }
+
+    return round($size, 2) . $delimiter . $units[$i];
+}
+
+/**
+ * 获取附件路径
+ * @param int $id 附件id
+ * @return string
+ */
+function get_file_path($id = 0)
+{
+    $path = model('attachment/Attachment')->getFilePath($id);
+    if (!$path) {
+        return __STATIC__ . 'admin/img/none.png';
+    }
+    return $path;
+}
+
+function get_thumb($id = 0)
+{
+    $path = model('attachment/Attachment')->getThumbPath($id);
+    if (!$path) {
+        return __STATIC__ . 'admin/img/none.png';
+    }
+    return $path;
+}
